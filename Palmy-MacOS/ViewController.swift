8450cd9d7d7ed78edea36c7cb473e703a1965381
Kirill Pukhov <kirillpukhov2004@gmail.com>
2023-12-09 19:55:12 +0300

    Add MacOS target

---
+++ Palmy-MacOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Palmy-MacOS
+//
+//  Created by Kirill Pukhov on 9.12.23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
--- Palmy-iOS/AuthViewController.swift
+++ Palmy-iOS/AuthViewController.swift
@@ -3,6 +3,62 @@ import OSLog
 import SnapKit
 import AlertKit
 
+enum AuthTextFieldType {
+    case login
+    case password
+}
+
+class AuthTextField: UITextField {
+    private let padding = UIEdgeInsets(top: 6, left: 14, bottom: 6, right: 14)
+
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    init(type: AuthTextFieldType) {
+        super.init(frame: .zero)
+
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+
+        let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .systemThinMaterialDark))
+        visualEffectView.isUserInteractionEnabled = false
+        insertSubview(visualEffectView, at: 0)
+        visualEffectView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        keyboardType = .default
+        autocapitalizationType = .none
+        autocorrectionType = .no
+        spellCheckingType = .no
+
+        switch type {
+        case .login:
+            returnKeyType = .continue
+
+            placeholder = "Login"
+        case .password:
+            returnKeyType = .done
+            isSecureTextEntry = true
+
+            placeholder = "Password"
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class AuthViewController: UIViewController {
     private let authController: AuthController
 
@@ -13,6 +69,8 @@ class AuthViewController: UIViewController {
     private var passwordTextField: AuthTextField!
 
     private var signInButton: UIButton!
+
+    private var signUpButton: UIButton!
 
     private var tapGestureRecognizer: UITapGestureRecognizer!
 
@@ -47,25 +105,31 @@ class AuthViewController: UIViewController {
         view.addSubview(containerStackView)
 
         loginTextField = AuthTextField(type: .login)
-        loginTextField.title = "Login"
         loginTextField.delegate = self
         containerStackView.addArrangedSubview(loginTextField)
-        containerStackView.setCustomSpacing(18, after: loginTextField)
+        containerStackView.setCustomSpacing(24, after: loginTextField)
 
         passwordTextField = AuthTextField(type: .password)
-        passwordTextField.title = "Password"
         passwordTextField.delegate = self
         containerStackView.addArrangedSubview(passwordTextField)
-        containerStackView.setCustomSpacing(26, after: passwordTextField)
+        containerStackView.setCustomSpacing(28, after: passwordTextField)
 
         signInButton = UIButton(type: .custom)
         signInButton.addTarget(self, action: #selector(signInButtonPressed), for: .touchUpInside)
         signInButton.setTitle("Sign In", for: .normal)
         signInButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .semibold)
-        signInButton.backgroundColor = UIColor(cgColor: CGColor(red: 23 / 255, green: 132 / 255, blue: 236 / 255, alpha: 1))
-        signInButton.layer.cornerRadius = 12.25
+        signInButton.backgroundColor = .systemBlue
+        signInButton.layer.cornerRadius = 10
         signInButton.layer.cornerCurve = .continuous
         containerStackView.addArrangedSubview(signInButton)
+        containerStackView.setCustomSpacing(6, after: signInButton)
+
+        signUpButton = UIButton(type: .custom)
+        signUpButton.addTarget(self, action: #selector(signUpButtonPressed), for: .touchUpInside)
+        signUpButton.setTitle("Sign Up", for: .normal)
+        signUpButton.titleLabel?.font = .systemFont(ofSize: 12, weight: .semibold)
+        signUpButton.setTitleColor(.systemBlue, for: .normal)
+        containerStackView.addArrangedSubview(signUpButton)
     }
 
     private func setupGestureRecognizers() {
@@ -80,17 +144,22 @@ class AuthViewController: UIViewController {
 
         loginTextField.snp.makeConstraints { make in
             make.width.equalTo(280)
-            make.height.equalTo(56)
+            make.height.equalTo(36)
         }
 
         passwordTextField.snp.makeConstraints { make in
             make.width.equalTo(280)
-            make.height.equalTo(56)
+            make.height.equalTo(36)
         }
 
         signInButton.snp.makeConstraints { make in
-            make.width.equalTo(203)
+            make.width.equalTo(280)
             make.height.equalTo(47)
+        }
+
+        signUpButton.snp.makeConstraints { make in
+            make.width.equalTo(47)
+            make.height.equalTo(28)
         }
     }
 
@@ -101,13 +170,13 @@ class AuthViewController: UIViewController {
 
         switch error {
         case AuthControllerError.invalidEmail:
-            alertView = AlertAppleMusic17View(title: "Invalid email", subtitle: nil, icon: .error)
+            alertView = AlertAppleMusic17View(title: "Invalid credantials", subtitle: nil, icon: .error)
         case AuthControllerError.wrongPassword:
-            alertView = AlertAppleMusic17View(title: "Wrong password", subtitle: nil, icon: .error)
+            alertView = AlertAppleMusic17View(title: "Invalid credantials", subtitle: nil, icon: .error)
         case AuthControllerError.tooManyRequests:
             alertView = AlertAppleMusic17View(title: "Too many attempts", subtitle: nil, icon: .error)
         case AuthControllerError.invalidLoginCredentials:
-            signUp()
+            alertView = AlertAppleMusic17View(title: "Invalid credantials", subtitle: nil, icon: .error)
         default: break
         }
 
@@ -115,8 +184,8 @@ class AuthViewController: UIViewController {
     }
 
     private func signIn() {
-        if let login = loginTextField.textField.text,
-           let password = passwordTextField.textField.text {
+        if let login = loginTextField.text, !login.isEmpty,
+           let password = passwordTextField.text, !password.isEmpty {
             authController.signIn(withEmail: login, password: password) { [weak self] error in
                 guard error == nil else {
                     if let error = error {
@@ -137,8 +206,8 @@ class AuthViewController: UIViewController {
     }
 
     private func signUp() {
-        if let login = loginTextField.textField.text,
-           let password = passwordTextField.textField.text {
+        if let login = loginTextField.text,
+           let password = passwordTextField.text {
             authController.signUp(withEmail: login, password: password) { [weak self] error in
                 guard error == nil else {
                     if let error = error {
@@ -161,17 +230,26 @@ class AuthViewController: UIViewController {
     // MARK: - Actions
 
     @objc private func signInButtonPressed() {
-        loginTextField.textField.resignFirstResponder()
-        passwordTextField.textField.resignFirstResponder()
-
+        loginTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        
+        Logger.general.log("\(#function)")
         signIn()
     }
 
+    @objc private func signUpButtonPressed() {
+        loginTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        
+        Logger.general.log("\(#function)")
+        signUp()
+    }
+
     @objc private func tapGestureRecognizerAction() {
-        if loginTextField.textField.isFirstResponder {
-            loginTextField.textField.resignFirstResponder()
-        } else if passwordTextField.textField.isFirstResponder {
-            passwordTextField.textField.resignFirstResponder()
+        if loginTextField.isFirstResponder {
+            loginTextField.resignFirstResponder()
+        } else if passwordTextField.isFirstResponder {
+            passwordTextField.resignFirstResponder()
         }
     }
 }
@@ -179,11 +257,24 @@ class AuthViewController: UIViewController {
 // MARK: - UITextFieldDelegate
 
 extension AuthViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text as? NSString else {
+            return true
+        }
+
+        let resultingText = text.replacingCharacters(in: range, with: string)
+
+        return true
+    }
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField === loginTextField.textField {
-            passwordTextField.textField.becomeFirstResponder()
-        } else if textField === passwordTextField.textField {
-            passwordTextField.textField.resignFirstResponder()
+        switch textField {
+        case loginTextField:
+            passwordTextField.becomeFirstResponder()
+        case passwordTextField:
+            passwordTextField.resignFirstResponder()
+        default:
+            break
         }
 
         return true
